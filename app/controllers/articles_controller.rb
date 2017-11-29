7063b7ef7b606eba69ea772d53e8889a9c815044
Anthony Torres <atorres@g2crowd.com>
2017-11-29 16:47:23 -0600

    Coming back

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -62,6 +62,7 @@ class ArticlesController < ApplicationController
   end
 
   private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
